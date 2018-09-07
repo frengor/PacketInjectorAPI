51218abae5fe4006c7ca1d647cf6953fff0bba2d
fren_gor <31711263+frengor@users.noreply.github.com>
2018-09-07 19:39:34 +0200

    Update PacketHandler.java

--- src/com/fren_gor/packetUtils/PacketHandler.java
+++ src/com/fren_gor/packetUtils/PacketHandler.java
@@ -1,8 +1,5 @@
 package com.fren_gor.packetUtils;
 
-import net.minecraft.util.io.netty.channel.ChannelHandlerContext;
-import net.minecraft.util.io.netty.channel.ChannelPromise;
-
 public interface PacketHandler {
 	
 }
